8048a01ce724b48f153bd2e059ec6a2ce9ca4355
alhaimouni <m.alhaimouni@gmail.com>
2025-05-20 00:33:32 +0300

    using useRouter() to navigate programmatically

--- app/news/[slug]/(.)image/page.js
+++ app/news/[slug]/(.)image/page.js
@@ -1,15 +1,26 @@
+"use client";
 import { DUMMY_NEWS } from "@/dummy-news";
+import { useRouter } from "next/navigation";
 
 export default function ImagePreviewPageDeep({ params }) {
+  const router = useRouter();
   const slugItem = params.slug;
   const imgx = DUMMY_NEWS.find((item) =>
     slugItem === item.slug ? item : null
   ).image;
   return (
     <>
-      <h1>This is intercepted route</h1>
-      <div>
-        <img src={`/images/news/${imgx}`} alt="image" />
+      <div
+        className="modal-backdrop"
+        onClick={() => {
+          router.back();
+        }}
+      >
+        <dialog className="modal" open>
+          <div className="fullscreen-image">
+            <img src={`/images/news/${imgx}`} alt="image" className="img" />
+          </div>
+        </dialog>
       </div>
     </>
   );
