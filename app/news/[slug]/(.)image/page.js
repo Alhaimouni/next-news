4bb48159e1ec4e5cee6d6d9dc45d678e8d432ca5
alhaimouni <m.alhaimouni@gmail.com>
2025-05-19 23:35:29 +0300

    add: intercepted route starting and testing for \/news\/slug\/image route

--- app/news/[slug]/(.)image/page.js
+++ app/news/[slug]/(.)image/page.js
@@ -1,12 +1,13 @@
 import { DUMMY_NEWS } from "@/dummy-news";
 
-export default function ImagePreviewPage({ params }) {
+export default function ImagePreviewPageDeep({ params }) {
   const slugItem = params.slug;
   const imgx = DUMMY_NEWS.find((item) =>
     slugItem === item.slug ? item : null
   ).image;
   return (
     <>
+      <h1>This is intercepted route</h1>
       <div>
         <img src={`/images/news/${imgx}`} alt="image" />
       </div>
